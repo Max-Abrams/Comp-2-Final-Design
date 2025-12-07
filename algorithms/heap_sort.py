b1fa386c30129df10b445c3d00ed844b14d7b479
joshua-hey <joshuaheymusic@gmail.com>
2025-12-07 09:50:59 -0500

    heap sorting added, main udpated

---
+++ algorithms/heap_sort.py
@@ -0,0 +1,43 @@
+# algorithms/heap_sort.py
+
+## Implementation of heap sort algo
+
+
+# Heapifies a subtree with root node 'i' which is an index in heap_array
+def heapifier(heap_array, n, i):
+    # largest value = root
+    largest = i
+    
+    # define left and right child indices
+    left = 2 * i + 1     
+    right = 2 * i + 2 
+
+    # check if each child exists and ensure the root is the largest value
+    if left < n and heap_array[i] < heap_array[left]:
+        largest = left
+    if right < n and heap_array[largest] < heap_array[right]:
+        largest = right
+
+    # if the largest isn't the root, change it, then recursively heapify the affected sub-tree
+    if largest != i:
+        heap_array[i], heap_array[largest] = heap_array[largest], heap_array[i]  # swap
+        heapifier(heap_array, n, largest)
+
+
+# Main function to do heap sort
+def heapSorter(heap_array):
+    n = len(heap_array) # define size of array
+
+    # Build heap or rearrange array
+    for i in range(n // 2 - 1, -1, -1): # start from last internal node and decrement to root
+        heapifier(heap_array, n, i)
+
+    # Extract elements 1 by 1
+    for i in range(n - 1, 0, -1): # decrement from last index to beginning
+
+        # Remove the max element and place at the end (sorted position)
+        heap_array[0], heap_array[i] = heap_array[i], heap_array[0]
+
+        # Call max heapify on the reduced heap (heap size is i now)
+        heapifier(heap_array, i, 0)
+    return heap_array
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from datastructures.hash_table import hash_table
 from datastructures.linked_list import linked_list
 from datastructures.bst import bst
 from algorithms.hash_search import lookup as hash_lookup
+from algorithms.heap_sort import heapSorter
 from Materials.material import Material
 from Materials.spacegroup import SpaceGroup
 from databases.MaterialDB import db
@@ -45,17 +46,37 @@ if __name__ == "__main__":
         test[0].display()
     else:
         print("Material not found")
+    print("\n")
 
-
+    ## test BST range query
     low, high = 5.4192, 5.4315
     results = density_bst.range_query(low, high)
 
-    print(f"BST range query: density between [{low}, {high}]")
+    print(f"BST range query: density between [{low} and {high}]")
     print("Total count of materials that satisfy the search criteria:", len(results))
     print("First 5 results:")
     for m in results[:5]:
         print(m.formula, m.density)
+    print("\n")
+    
+    
+    ## test HeapSort on densities
+    density_list = []
+    for i, row in db.df.iterrows():
+        mat = Material(row)
+        density_list.append((mat.density, mat.data_id, mat)) # ID added to (arbitrarily) break ties (ties=errors)
 
+    sorted_pairs = heapSorter(density_list)
+
+    printnum = 15
+    # find the longest formula so alignment is even
+    max_formula_len = max(len(mat.formula) for _, _, mat in sorted_pairs[:printnum])
+
+    # print formatted output
+    print(f"Heapsort: {printnum} lowest density materials:")
+    for density, jid, mat in sorted_pairs[:printnum]:
+        print(f"{mat.formula:<{max_formula_len}}  {density:>10.4f}")
+    print("\n")
 
 
     #Bloom search
